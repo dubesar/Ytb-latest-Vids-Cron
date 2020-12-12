e265414ca97c005d93ac5e8ad85fbf502997a55d
Sarvesh Dubey <dubeysarvesh5525@gmail.com>
2020-12-12 10:21:23 +0530

    Update api_test.py

--- api_test.py
+++ api_test.py
@@ -21,7 +21,7 @@ def latestVideoYtb():
 
     api_service_name = "youtube"
     api_version = "v3"
-    DEVELOPER_KEY = "AIzaSyDbmlML9n13pZCx_qpTTPx8FR7D6sBsyVc"
+    DEVELOPER_KEY = "ENTER YOUR API KEY"
 
     youtube = googleapiclient.discovery.build(
         api_service_name, api_version, developerKey = DEVELOPER_KEY)
